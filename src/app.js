32d88d5b9ee6b0da82573d3432142738eef88ba1
shivani_simpleDev <shivi.solanki29@gmail.com>
2024-11-20 06:14:44 +0100

    create http method routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ const express = require("express");
 
 const app = new express();
 
-//handle the request and send back response
-
-app.use((req, res) => {
-  res.send("Hello added try with new ");
+app.get("/hello", (req, res) => {
+  res.send({
+    firstname: "Node",
+    lastname: "JS",
+  });
+});
+app.post("/hello", (req, res) => {
+  res.send("Successfully update db");
 });
 
+app.delete("/hello/:userid/:name/:password", (req, res) => {
+  res.send("record delete");
+});
 //api /route handler
 app.use("/test", (req, res) => {
   res.send("testnodemon Hello added");
@@ -15,3 +22,29 @@ app.use("/test", (req, res) => {
 app.listen(3000, () => {
   console.log("hello from server");
 });
+/*
+- season 02
+  -episode04
+  //const express = require("express");
+
+const app = new express();
+
+//handle the request and send back response
+
+// app.use((req, res) => {
+// res.send("Hello added try with new ");
+// });
+
+app.use("/hello", (req, res) => {
+res.send("Hello 2332 Hello Hello");
+});
+
+//api /route handler
+app.use("/test", (req, res) => {
+res.send("testnodemon Hello added");
+});
+app.listen(3000, () => {
+console.log("hello from server");
+});
+
+*/
