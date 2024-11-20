7107fc69be65b2873b4d025efe2b8b9055b59c90
shivani_simpleDev <shivi.solanki29@gmail.com>
2024-11-20 14:10:42 +0100

    multiple route handlers, next()

--- homework.md
+++ homework.md
@@ -1,1 +1,9 @@
 - create a respository
+
+-Episode 05
+-Multiple route Handlers
+-next()
+-next function and errors along with res.send()
+
+- app.use("/route",rH1, rH2,rH3,rH4,rH5,rH6)
+  time- 1:10:25 remaining
--- src/app.js
+++ src/app.js
@@ -2,24 +2,31 @@ const express = require("express");
 
 const app = new express();
 
-app.get("/hello", (req, res) => {
-  console.log(req.query.user);
-  res.send({
-    firstname: "Node",
-    lastname: "JS",
-  });
-});
-app.post("/hello", (req, res) => {
-  res.send("Successfully update db");
-});
+app.use(
+  "/hello",
+  (err, eq, res, next) => {
+    console.log("Handling route user 01");
+    next();
+    res.send("response 01");
+  },
+  [
+    (req, res, next) => {
+      console.log("Handling route user 02");
+      next();
+      res.send("2nd response!");
+    },
+    (req, res, next) => {
+      console.log("Handling route user 03");
+      next();
+      res.send("3rd response!");
+    },
+  ],
+  (req, res, next) => {
+    console.log("Handling route user 04");
+    res.send("4th response!");
+  }
+);
 
-app.delete("/hello", (req, res) => {
-  res.send("record delete");
-});
-//api /route handler
-app.use("/test", (req, res) => {
-  res.send("testnodemon Hello added");
-});
 app.listen(3000, () => {
   console.log("hello from server");
 });
