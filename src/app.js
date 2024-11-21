b045d3c6a4147300e890b13364336952bf8ceb99
shivani_simpleDev <shivi.solanki29@gmail.com>
2024-11-21 11:35:04 +0100

    error handlig in try..catch and app.use()

--- src/app.js
+++ src/app.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const app = new express();
 
-const {
-  adminAuthentication,
-  userAuthentication,
-} = require("./middleware/auth.js");
+//Error Handling ways
 
-app.use("/admin", adminAuthentication);
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
+});
+app.get("/getuserdata", (req, res) => {
+  //2nd way to handling error using try..catch block and it is best way
 
-app.get("/user", userAuthentication, (req, res) => {
-  res.send("User login succesfully");
+  try {
+    //logic for call DB and get userdata
+    throw new Error("throw error from route");
+    res.send("user data sent");
+  } catch (err) {
+    res.status(500).send("some error occure contact support team");
+  }
 });
 
-app.get("/admin/allusers", (req, res) => {
-  res.send("send the data");
-});
+//1 way to handle error
+//"/" means match all routes
 
-app.delete("/admin/delete", (req, res) => {
-  res.send("deleted a user");
+//if you write this code first not get below response because not get any error ,
+//error got in app.get() handle function
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
 });
 
 app.listen(3000, () => {
