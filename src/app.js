c129f77e12066345cfea0ca5b66656c2d86cfb62
shivani_simpleDev <shivi.solanki29@gmail.com>
2024-11-24 05:32:43 +0100

    ep-7 Patch,delete,get Apis

--- src/app.js
+++ src/app.js
@@ -1,28 +1,67 @@
 const express = require("express");
 const { connectDB } = require("./config/database.js");
 const { Users } = require("./Models/User.js");
-
 const app = new express();
 
-app.post("/signup", async (req, res) => {
-  //create instace of model xyz
-  const user = new Users({
-    firstName: "Shardha",
-    lastname: "Kapoor",
-    email: "shardha@k.com",
-    password: "shardha@123",
-  });
+// Middleware to handle JSON
+const sm = app.use(express.json());
+//read data from db
 
+//get user by applying fliter
+app.get("/user", async (req, res) => {
+  // console.log(req.body._id);
   try {
-    // throw new error("xyz");
-    await user.save();
-    //res.send("Add user successfully");
-    res.send(`${user.firstName} successfully added`);
+    const FindUser = await Users.findOne({ firstName: "Sonam" });
+    console.log("user with filter :" + FindUser);
+    res.send("got data user by filter ");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
   }
 });
 
+//get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await Users.find({});
+    console.log("all users details:" + allUsers);
+    res.send("Get all users data");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+app.delete("/user", async (req, res) => {
+  console.log("Hello from delete");
+  const userdeleteId = req.body.userId;
+  console.log(userdeleteId);
+  try {
+    await Users.findByIdAndDelete(userdeleteId);
+    res.send("user delete");
+  } catch (err) {
+    res.status(400).send("Error delete the user:" + err.message);
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const user = new Users(req.body);
+  try {
+    // throw new error("xyz");
+    await user.save();
+    //res.send("Add user successfully");
+    res.send(`${user.firstName} successfully added in DB`);
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await Users.findByIdAndUpdate(userId, { email: req.body.email });
+    res.send(`${req.body.email} successfully updated in DB`);
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
 connectDB()
   .then(() => {
     console.log("succesfully connected DB");
