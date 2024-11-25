2e60635bd101ad6986a68c849b09f931900105dc
shivani_simpleDev <shivi.solanki29@gmail.com>
2024-11-25 17:42:18 +0100

    ep-08 Data seitization api level and schema level

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,13 +1,57 @@
 const { default: mongoose, model, Schema } = require("mongoose");
+const { default: isEmail } = require("validator/lib/isEmail");
+const validator = require("validator");
 
-const UserSchema = new Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  email: { type: String },
-  password: { type: String },
-  age: { type: Number },
-  gender: { type: String },
-});
+const UserSchema = new Schema(
+  {
+    firstName: { type: String, required: true, minLength: 3, trim: true },
+    lastName: { type: String, trim: true },
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      trim: true,
+      unique: true,
+      validate(value) {
+        console.log("value from email schema" + value);
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid emailid " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password");
+        }
+      },
+    },
+    age: { type: Number, min: "18", trim: true },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://kobikoachman.com/wp-content/uploads/2018/03/dummyProfile-male.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid URL ");
+        }
+      },
+    },
+    skills: [String],
+  },
+  { timestamps: true }
+);
 
 const Users = new model("UserModels", UserSchema);
 //const users = mongoose.model(users,UserSchema)
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { connectDB } = require("./config/database.js");
 const { Users } = require("./Models/User.js");
+const validator = require("validator");
 const app = new express();
 
 // Middleware to handle JSON
@@ -43,25 +44,49 @@ app.delete("/user", async (req, res) => {
 
 app.post("/signup", async (req, res) => {
   const user = new Users(req.body);
+  const data = req.body;
   try {
+    // console.log(validator.isEmail(req.body.email));
+    if (!validator.isEmail(data.email)) {
+      throw new Error("Invalid emailid " + data.email);
+    }
+
+    if (!validator.isStrongPassword(data.password)) {
+      throw new Error("Enter a strong password");
+    }
     // throw new error("xyz");
     await user.save();
     //res.send("Add user successfully");
     res.send(`${user.firstName} successfully added in DB`);
   } catch (err) {
-    res.status(400).send("Error saving the user:" + err.message);
+    res.status(400).send("Error saving the user: " + err.message);
   }
 });
 
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  console.log(data);
   try {
-    await Users.findByIdAndUpdate(userId, { email: req.body.email });
-    res.send(`${req.body.email} successfully updated in DB`);
+    const Allowed_Updates = ["age", "skills", "photoUrl", "gender"];
+    const IsUpdateAllowed = Object.keys(data).every((k) =>
+      Allowed_Updates.includes(k)
+    );
+    if (!IsUpdateAllowed) {
+      throw new Error("Update Not allowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("skills Not be more then 10");
+    }
+    await Users.findByIdAndUpdate(userId, data, { runValidators: true });
+    res.send(`${data} successfully updated in DB`);
   } catch (err) {
-    res.status(400).send("Error saving the user:" + err.message);
+    res.status(400).send("Error saving the user: " + err.message);
   }
 });
+
 connectDB()
   .then(() => {
     console.log("succesfully connected DB");
